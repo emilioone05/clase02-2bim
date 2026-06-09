0cc45b6927c528149a2ab5c68dfd0e212c9dd2dd
EmilioOne05 <emiliojosepe05@gmail.com>
2026-06-09 12:43:51 -0500

    middle

--- orm/agregar_actores.py
+++ orm/agregar_actores.py
@@ -2,31 +2,33 @@ import csv
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 
-# se importa la clase(s) del archivo genera_tablas
-from modelo import Pais, Plataforma, Serie, Actor, Premio
-
-# se importa información del archivo de configuración
+from modelo import Pais, Serie, Actor
 from config import cadena_base_datos
 
-# se genera el enlace al gestor de base de datos
 engine = create_engine(cadena_base_datos)
-
 Session = sessionmaker(bind=engine)
 session = Session()
+
 def leer_csv(ruta):
     with open(ruta, encoding='utf-8') as f:
         lector = csv.DictReader(f)
         return list(lector)
-# 1. Actores
+
 print("Cargando Actores...")
 for fila in leer_csv('../data/actores.csv'):
-    session.add(Serie(
-        id=int(fila['id']),
-        nombre=(fila['nombre']),
-        edad=(fila['edad']),
-        pais=(fila['pais']),
-        serie=(fila['serie']),
 
+    pais_obj = session.query(Pais).filter_by(nombre=fila['pais']).first()
+
+    serie_obj = session.query(Serie).filter_by(titulo=fila['serie']).first()
+
+    session.add(Actor(
+        id     = int(fila['id']),
+        nombre = fila['nombre'],
+        edad   = int(fila['edad']),
+        pais   = pais_obj,
+        serie  = serie_obj
     ))
+
 session.commit()
 session.close()
+print("Actores cargados correctamente.")
--- orm/agregar_premios.py
+++ orm/agregar_premios.py
@@ -2,31 +2,31 @@ import csv
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 
-# se importa la clase(s) del archivo genera_tablas
-from modelo import Pais, Plataforma, Serie, Actor, Premio
-
-# se importa información del archivo de configuración
+from modelo import Serie, Premio
 from config import cadena_base_datos
 
-# se genera el enlace al gestor de base de datos
 engine = create_engine(cadena_base_datos)
-
 Session = sessionmaker(bind=engine)
 session = Session()
+
 def leer_csv(ruta):
     with open(ruta, encoding='utf-8') as f:
         lector = csv.DictReader(f)
         return list(lector)
-# 1. Premio
-print("Cargando Premio...")
-for fila in leer_csv('../data/premios.csv'):
-    session.add(Serie(
-        id=int(fila['id']),
-        nombre_premio=(fila['nombre_premio']),
-        categoria=(fila['categoria']),
-        anio=(fila['anio']),
-        serie=(fila['serie']),
 
+print("Cargando Premios...")
+for fila in leer_csv('../data/premios.csv'):
+
+    serie_obj = session.query(Serie).filter_by(titulo=fila['serie']).first()
+
+    session.add(Premio(
+        id            = int(fila['id']),
+        nombre_premio = fila['nombre_premio'],
+        categoria     = fila['categoria'],
+        anio          = int(fila['anio']),
+        serie         = serie_obj
     ))
+
 session.commit()
 session.close()
+print("Premios cargados correctamente.")
